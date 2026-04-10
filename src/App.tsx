7385b2b2dd760ff51720ceb35c564f3fed80e562
Tsvetomir Dimitrov <ts.dimitrov93@gmail.com>
2026-04-10 15:38:29 +0300

    fixed not found and cv

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import CvPage from './pages/cv/CvPage'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/build-cv-hidden" element={<CvPage />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
